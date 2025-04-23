7f7feeb432a5927a57a867b8b552b4a9937bdd4e
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2025-04-24 00:13:23 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  assetPrefix: isProd ? '/alvrian.github.io/' : '',
-  basePath: isProd ? '/alvrian.github.io' : '',
-  output: 'export',
-};
-
-export default nextConfig;
+export default {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+    output: 'export',
+  };
