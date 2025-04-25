2dda82c344c93883372786c73fd43ec0f3d50417
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2025-04-25 23:41:21 +0700

    test build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SunIcon from "./assets/sun.svg";
+import  MoonIcon from "./assets/moon.svg"
 
 function App() {
+  const [lightMode, setLightMode] = useState(false);
+  
+  useEffect(() => {
+    if(lightMode){
+      document.body.classList.add("light-mode");
+    }else{
+      document.body.classList.remove("light-mode")
+    }
+  }, [lightMode]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='snap-section'>
+        <div className='header'>
+            <label className="switch">
+              <input type="checkbox" checked={!lightMode} onChange={() => setLightMode(!lightMode)}/>
+              <span className="slider"></span>
+            </label>
+            <span className="mode-icon">
+              <img 
+                key={lightMode ? "sun" : "moon"} src={lightMode ? SunIcon : MoonIcon} 
+                alt={lightMode ? "Sun" : "Moon"} className="mode-icon"
+              />
+            </span>
+          </div>
+          <h2 className='mainText'>Alvrian Timotius</h2>
+      </div>
+      <div className='snap-section'>
+        <div className='header'>
+            <label className="switch">
+              <input type="checkbox" checked={!lightMode} onChange={() => setLightMode(!lightMode)}/>
+              <span className="slider"></span>
+            </label>
+            <span className="mode-icon">
+              <img 
+                key={lightMode ? "sun" : "moon"} src={lightMode ? SunIcon : MoonIcon} 
+                alt={lightMode ? "Sun" : "Moon"} className="mode-icon"
+              />
+            </span>
+          </div>
+          <h2 className='mainText'>Alvrian Timotius</h2>
+      </div>
     </div>
   );
 }
