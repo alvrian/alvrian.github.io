2158844263f6b2c96f97c18c6576e6c78baae3b9
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2025-04-25 22:14:10 +0700

    reset

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
-    output: 'export',
-  };
