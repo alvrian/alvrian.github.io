270ed1805701909dea39a6d72738cd2f12a958f1
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2025-04-28 22:24:05 +0700

    c

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Other from './other';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter>
     <Routes>
         <Route path="/" element={<App />} />
         <Route path="*" element={<Other />} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
+    <App />
   </React.StrictMode>
 );
 
