b56543da96c179b9d9feef6f3efa2b6aa8a5656b
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2025-05-04 15:59:28 +0700

    add: dir, style

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import GithubIcon from "./assets/github-logo.svg";
 import BottomArrow from "./assets/arrow-bottom.svg";
 import Dino from "./assets/Dino-gray.png";
 import Card from "./Component/Card";
-import ACA from "./assets/ACA.webp";
-import DL from "./assets/DL-Funda.webp"
-import UD from "./assets/udemy.webp";
+import ACA from "./assets/courses/ACA.webp";
+import DL from "./assets/courses/DL-Funda.webp"
+import UD from "./assets/courses/udemy.webp";
 import Game from "./assets/game.png";
-import Corn from "./assets/CORN.png";
-import NLP from "./assets/nvidia-nlp.png";
+import Corn from "./assets/media/CORN.png";
+import NLP from "./assets/courses/nvidia-nlp.png";
+import CV from "./assets/media/AOL-compvis.png";
+import Fitter from "./assets/media/fitter-screen.png";
+import SC from "./assets/media/stepcode.png";
+import ScDemo from "./assets/demo/STEPCODE-Demo.mp4";
+import FitterDemo from "./assets/demo/Fitter-Demo.mp4";
+import CvDemo from "./assets/demo/CV-demo.mp4";
 
 function App() {
   const [lightMode, setLightMode] = useState(false);
@@ -100,21 +106,13 @@ function App() {
         </div>
         <h2 className="mainText">Alvrian Timotius</h2>
         <div className="profile-links">
-          <a
-            href="https://www.linkedin.com/in/alvrian-timotius/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://www.linkedin.com/in/alvrian-timotius/" target="_blank" rel="noopener noreferrer">
             <button className="profile-button">
               <img src={LinkedinIcon} className="profile-icon" alt="LinkedIn" />
               <span>LinkedIn</span>
             </button>
           </a>
-          <a
-            href="https://github.com/alvrian"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://github.com/alvrian" target="_blank" rel="noopener noreferrer">
             <button className="profile-button">
               <img src={GithubIcon} className="profile-icon" alt="GitHub" />
               <span>GitHub</span>
@@ -126,16 +124,11 @@ function App() {
             <span className="title" style={{ fontWeight: "bold" }}>
               Application Developer | Computer Science Student
             </span>
-            <br />
-            <br />
-            Computer Science student at Binus University, specializing in
-            Software Engineering and Machine Learning. As part of the Binus
-            Master Track program, set to graduate with both a Bachelor's and
-            Master’s degree in the near future. I have experience in developing
-            machine learning models and building software applications, using
-            frameworks like Laravel, React, and Express. Currently, I’m
-            interning as an applications developer at Indonesia's largest
-            private bank, gaining hands-on experience in real-world projects.
+            <br /><br />
+            Computer Science student at Binus University, specializing in Software Engineering and Machine Learning. As part of the Binus
+            Master Track program, set to graduate with both a Bachelor's and Master’s degree in the near future. I have experience in developing
+            machine learning models and building software applications, using frameworks like Laravel, React, and Express. Currently, I’m
+            interning as an applications developer at Indonesia's largest private bank, gaining hands-on experience in real-world projects.
           </p>
         </div>
       </div>
@@ -154,21 +147,37 @@ function App() {
             <Card 
               title="Sea-Land Segmentation" 
               category = "Machine Learning"
-              desc = "Sea-Land Segmentation based on Superpixel Fuzzy C-Means Clustering and Modified Chan-Vese Model From Optical Images"
-              media = {ACA} 
+              desc = "Sea-Land Segmentation based on Superpixel Fuzzy C-Means Clustering and Modified Chan-Vese Model From Optical Images. This is a project created for Computer Vision course that I took during 5th semester. I work with 3 other members to create this project. We use Superpixel Fuzzy C-Means Clustering and Modified Chan-Vese Model to segment the image into sea and land. We use Python and OpenCV to create this project. We also use Streamlit to create the web app."
+              media = {CV} 
               repo = "https://github.com/alvrian/compvis-deploy"
+              demo = {CvDemo}
             />
             <Card 
               title="FarmByte" 
               category = "App Development"
               media = {Corn} 
-              desc = "Welcome to FarmByte, an innovative platform designed to create a sustainable and mutually beneficial ecosystem by connecting farmers, compost producers, and restaurants. Our application promotes responsible consumption and production, aligning directly with SDG 12: Responsible Consumption and Production, which emphasizes efficient resource use, waste reduction, and sustainable practices."/>
+              desc = "Welcome to FarmByte, an innovative platform designed to create a sustainable and mutually beneficial ecosystem by connecting farmers, compost producers, and restaurants. Our application promotes responsible consumption and production, aligning directly with SDG 12: Responsible Consumption and Production, which emphasizes efficient resource use, waste reduction, and sustainable practices."
+              repo = "https://github.com/alvrian/project-web-prog"
+              link = "https://alvrian-compvis.streamlit.app/"
+            />
             <Card 
-              title="test3 title model"
-              media = {UD} 
-              desc = "this is a test project to test this pop up ability this is a test project to test this pop up ability Lorem ipsum it is"/>
-            <Card title="test4 title model" desc = "this is a test project to test this pop up ability this is a test project to test this pop up ability Lorem ipsum it is"/>
-            <Card title="test5" desc = "this is a test project to test this pop up ability this is a test project to test this pop up ability Lorem ipsum it is"/>
+              title="Fitter"
+              media = {Fitter} 
+              desc = "Fitter is a Deep Learning created to recognize the type of food and its nutritional value. This is a collaborative project developed for Machine Learning course's final project. I work with 2 other members to create this project."
+              repo = "https://github.com/tiffanyjoycelyn/fitter"
+              demo= {FitterDemo}
+            />
+            <Card 
+              title="STEPCODE" 
+              desc = "STEPCODE is a collaborative project developed by me and four other members as part of our Software Engineering course. We created a platform to help individuals learn how to code, using technologies like typescript, React with Vite, Express, and Firebase. The unique feature of this platform is that each individuals can choose their own tutor based on others experience. Our team followed the SCRUM framework, organizing our work into sprints and holding daily standups to ensure everyone was aligned. We collaborated closely with a designated Product Owner who managed the backlog, and held sprint reviews to gather feedback on our progress."
+              media = {SC}
+              repo = "https://github.com/alvrian/AOL_SoftEng"
+              demo = {ScDemo}
+            />
+            <Card 
+              title="test5" 
+              desc = "this is a test project to test this pop up ability this is a test project to test this pop up ability Lorem ipsum it is"
+            />
 
           </div>
           <button
--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Card.css";
+import GithubIcon from "../assets/github-logo.svg";
 
 export default function Card({
   title,
@@ -8,7 +9,7 @@ export default function Card({
   link,
   category,
   repo,
-  additional,
+  demo,
 }) {
   const [isPopupVisible, setPopupVisible] = useState(false);
 
@@ -28,7 +29,6 @@ export default function Card({
           <br />
           <p className="card-category">{category}</p>
         </p>
-
         <p className="card-description">{desc}</p>
         <img className="card-media" src={media} alt="..." />
       </div>
@@ -40,7 +40,22 @@ export default function Card({
               {title}<br />
               <p className="card-category">{category}</p>
             </p>
-            <img className="card-media" src = {media} alt = "loading..."/>
+            <div className="card-media-container">
+              <div>
+                <video className="card-media" controls autoplay>
+                  <source src={demo} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+              <div className="card-button" >
+                <a href= {repo} target="_blank" rel="noopener noreferrer">
+                  <button className="git-button">
+                    <img src={GithubIcon} className="profile-icon" alt="GitHub" />
+                    <span>GitHub</span>
+                  </button>
+                </a> 
+              </div>
+            </div>
             <p>{desc}</p>
             <div className="closeButtonContainer">
                 <button className="closeButton" onClick={handleClosePopup}>Close</button>
